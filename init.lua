d7b47e956f8dc91a2e7a3d8dee834b6a88d3b2b2
Tucker Bamberg <mootpoint23@gmail.com>
2016-11-16 09:23:58 -0500

    update to PG
    removed Justice Mod addition

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- Minetest 0.4.10+ mod: chat_anticurse
--- punish player for cursing by disconnecting them in increments ending ultimately in permanent ban
+-- remind player chat should stay pg. no punishment for cursing.
 --
 --  Created in 2015 by Andrey. 
 --  This mod is Free and Open Source Software, released under the LGPL 2.1 or later.
@@ -14,7 +14,7 @@ chat_anticurse.simplemask = {}
 -- eventually to be exported in cleartext to a .txt file
 -- french and spanish curse words courtesy of mok
 -- note from mok cojer in MExico means the F word, but in peru cojer means to take and is common dialect. not added to this list for that reason
--- due partially to laziness and partially to the fact that i am going to be transcoding these to /txt soon, franch and spanish words will not be masked
+-- due partially to laziness and partially to the fact that i am going to be transcoding these to /txt soon, french and spanish words will not be masked
 
 
 local x1 = 'a'
@@ -28,84 +28,58 @@ local y3 = 'о'
 local y4 = 'е'
 local y5 = 'я'
 
-chat_anticurse.simplemask[1] = ' '..x1..'s' .. 's '
-chat_anticurse.simplemask[2] = ' d' .. ''..x2..'ck'
-chat_anticurse.simplemask[3] = ' p'..x4..'n' .. 'is'
-chat_anticurse.simplemask[4] = ' p' .. ''..x3..'ssy'
-chat_anticurse.simplemask[5] = ' h'..x5..'' .. 'r'..'ny '
-chat_anticurse.simplemask[6] = ' b'..x2..'' .. 'tch'
-chat_anticurse.simplemask[7] = ' b'..x2..'' .. 'tche'
-chat_anticurse.simplemask[8] = ' s'..x4..'' .. 'x'
-chat_anticurse.simplemask[9] = ' '..y4..'б' .. 'а'
-chat_anticurse.simplemask[10] = ' бл'..y5..'' .. ' '
-chat_anticurse.simplemask[11] = ' ж' .. ''..y3..'п'
-chat_anticurse.simplemask[12] = ' х' .. ''..y1..'й'
-chat_anticurse.simplemask[13] = ' ч' .. 'л'..y4..'н'
-chat_anticurse.simplemask[14] = ' п'..y2..'' .. 'зд'
-chat_anticurse.simplemask[15] = ' в'..y3..'' .. 'збуд'
-chat_anticurse.simplemask[16] = ' в'..y3..'з' .. 'б'..y1..'ж'
-chat_anticurse.simplemask[17] = ' сп'..y4..'' .. 'рм'
-chat_anticurse.simplemask[18] = ' бл'..y5..'' .. 'д'
-chat_anticurse.simplemask[19] = ' бл'..y5..'' .. 'ть'
-chat_anticurse.simplemask[20] = ' с' .. ''..y4..'кс'
-chat_anticurse.simplemask[21] = 'f' .. ''..x3..'ck'
-chat_anticurse.simplemask[22] = ''..x1..'rs'..x4..'h'..x5..'l'..x4..''
-chat_anticurse.simplemask[23] = ' c'..x3..'nt '
-chat_anticurse.simplemask[24] = ' '..x1..'s' .. 'sh'..x5..'le'
-chat_anticurse.simplemask[25] = ' h'..x4..'ll'
-chat_anticurse.simplemask[26] = 'n00b'
-chat_anticurse.simplemask[27] = 'noob'
-chat_anticurse.simplemask[28] = 'stupid'
-chat_anticurse.simplemask[29] = 'hate'
-chat_anticurse.simplemask[30] = 'tabarnak'
-chat_anticurse.simplemask[31] = 'osti'
-chat_anticurse.simplemask[32] = 'calisse'
-chat_anticurse.simplemask[33] = 'câlisse'
-chat_anticurse.simplemask[34] = 'viarge'
-chat_anticurse.simplemask[35] = 'criss'
-chat_anticurse.simplemask[36] = 'crisse'
-chat_anticurse.simplemask[37] = 'putain'
-chat_anticurse.simplemask[38] = 'pute'
-chat_anticurse.simplemask[39] = 'salope'
-chat_anticurse.simplemask[40] = 'nique ta mère'
-chat_anticurse.simplemask[41] = 'pétasse'
-chat_anticurse.simplemask[42] = 'enfoiré'
-chat_anticurse.simplemask[43] = 'enfoirée'
-chat_anticurse.simplemask[44] = 'sacrament'
-chat_anticurse.simplemask[45] = 'simonaque'
-chat_anticurse.simplemask[46] = 'merde'
-chat_anticurse.simplemask[47] = 'marde'
-chat_anticurse.simplemask[48] = 'imbécile'
-chat_anticurse.simplemask[49] = 'idiot'
-chat_anticurse.simplemask[50] = 'pénis'
-chat_anticurse.simplemask[51] = 'sexe'
-chat_anticurse.simplemask[52] = 'baise'
-chat_anticurse.simplemask[53] = 'bâtard'
-chat_anticurse.simplemask[54] = 'batard'
-chat_anticurse.simplemask[55] = 'puta'
-chat_anticurse.simplemask[56] = 'puto'
-chat_anticurse.simplemask[57] = 'concha su madre'
-chat_anticurse.simplemask[58] = 'concha tu madre'
-chat_anticurse.simplemask[59] = 'perra'
-chat_anticurse.simplemask[60] = 'pinche'
-chat_anticurse.simplemask[61] = 'pendejo'
-chat_anticurse.simplemask[62] = 'sexo'
-chat_anticurse.simplemask[63] = 'maricon'
-chat_anticurse.simplemask[64] = 'joto'
-chat_anticurse.simplemask[65] = 'bastardo'
-chat_anticurse.simplemask[66] = 'culo'
-chat_anticurse.simplemask[67] = 'verga'
-chat_anticurse.simplemask[68] = 'mierda'
-chat_anticurse.simplemask[69] = 'chinga'
-chat_anticurse.simplemask[70] = 'chingados'
-chat_anticurse.simplemask[71] = 'jode'
-chat_anticurse.simplemask[72] = 'faggot'
+chat_anticurse.simplemask[1] = ' d' .. ''..x2..'ck'
+chat_anticurse.simplemask[2] = ' p'..x4..'n' .. 'is'
+chat_anticurse.simplemask[3] = ' p' .. ''..x3..'ssy'
+chat_anticurse.simplemask[4] = ' b'..x2..'' .. 'tch'
+chat_anticurse.simplemask[5] = ' b'..x2..'' .. 'tche'
+chat_anticurse.simplemask[6] = ' s'..x4..'' .. 'x'
+chat_anticurse.simplemask[7] = ' '..y4..'б' .. 'а'
+chat_anticurse.simplemask[8] = ' бл'..y5..'' .. ' '
+chat_anticurse.simplemask[9] = ' ж' .. ''..y3..'п'
+chat_anticurse.simplemask[10] = ' х' .. ''..y1..'й'
+chat_anticurse.simplemask[11] = ' ч' .. 'л'..y4..'н'
+chat_anticurse.simplemask[12] = ' п'..y2..'' .. 'зд'
+chat_anticurse.simplemask[13] = ' в'..y3..'' .. 'збуд'
+chat_anticurse.simplemask[14] = ' в'..y3..'з' .. 'б'..y1..'ж'
+chat_anticurse.simplemask[15] = ' сп'..y4..'' .. 'рм'
+chat_anticurse.simplemask[16] = ' бл'..y5..'' .. 'д'
+chat_anticurse.simplemask[17] = ' бл'..y5..'' .. 'ть'
+chat_anticurse.simplemask[18] = ' с' .. ''..y4..'кс'
+chat_anticurse.simplemask[19] = 'f' .. ''..x3..'ck'
+chat_anticurse.simplemask[20] = ' c'..x3..'nt '
+chat_anticurse.simplemask[21] = ' '..x1..'s' .. 'sh'..x5..'le'
+chat_anticurse.simplemask[22] = 'caliss'
+chat_anticurse.simplemask[23] = 'câliss'
+chat_anticurse.simplemask[24] = 'viarge'
+chat_anticurse.simplemask[25] = 'criss'
+chat_anticurse.simplemask[26] = 'crisse'
+chat_anticurse.simplemask[27] = 'pute'
+chat_anticurse.simplemask[28] = 'salope'
+chat_anticurse.simplemask[29] = 'nique ta mère'
+chat_anticurse.simplemask[30] = 'pétasse'
+chat_anticurse.simplemask[31] = 'enfoiré'
+chat_anticurse.simplemask[32] = 'enfoirée'
+chat_anticurse.simplemask[33] = 'baise'
+chat_anticurse.simplemask[34] = 'bâtard'
+chat_anticurse.simplemask[35] = 'batard'
+chat_anticurse.simplemask[36] = 'puta'
+chat_anticurse.simplemask[37] = 'puto'
+chat_anticurse.simplemask[38] = 'concha su madre'
+chat_anticurse.simplemask[39] = 'concha tu madre'
+chat_anticurse.simplemask[40] = 'perra'
+chat_anticurse.simplemask[41] = 'pinche'
+chat_anticurse.simplemask[42] = 'pendejo'
+chat_anticurse.simplemask[43] = 'maricon'
+chat_anticurse.simplemask[44] = 'joto'
+chat_anticurse.simplemask[45] = 'bastardo'
+chat_anticurse.simplemask[46] = 'verga'
+chat_anticurse.simplemask[47] = 'chinga'
+chat_anticurse.simplemask[48] = 'chingados'
+chat_anticurse.simplemask[49] = 'jode'
+chat_anticurse.simplemask[50] = 'faggot'
 
 
-
-local judge_name = 'Server'
-local seconds = '30'
-local cause = 'Cursing or Hate Speech'
 
 chat_anticurse.check_message = function(name, message)
     local checkingmessage=string.lower( name..' '..message ..' ' )
@@ -116,33 +90,18 @@ chat_anticurse.check_message = function(name, message)
             break
         end
     end
-    
-    --additional checks
-    if 
-        string.find(checkingmessage, ' c'..x3..'' .. 'm ', 1, true) ~=nil and 
-        not (string.find(checkingmessage, ' c'..x3..'' .. 'm ' .. 'se', 1, true) ~=nil) and
-        not (string.find(checkingmessage, ' c'..x3..'' .. 'm ' .. 'to', 1, true) ~=nil)
-    then
-        uncensored = 2
-    end
     return uncensored
 end
 
 minetest.register_on_chat_message(function(name, message)
     local uncensored = chat_anticurse.check_message(name, message)
-    if uncensored == 1 then
-        justice.sentence(judge_name, name, tonumber(seconds), cause)
-        minetest.log('action', 'Player '..name..' jailed for '..cause..'. Chat:'..message)
-        return true
+    if uncensored == 1 or uncensored == 2 then
+        minetest.chat_send_player(name, 'Please help us keep the chat rated PG, if you would like to curse use /channels')
+        minetest.log('action', 'Player '..name..' warned for cursing. Chat:'..message)
+        return false
     end
 
-    if uncensored == 2 then
-        justice.sentence(judge_name, name, tonumber(seconds), cause)
-        -- minetest.chat_send_all('Player <'..name..'> jailed for '..cause..'.' )
-        minetest.log('action', 'Player '..name..' warned for '..cause..'. Chat:'..message)
-        return true
-    end
-
+    
 end)
 
 if minetest.chatcommands['me'] then
@@ -150,41 +109,12 @@ if minetest.chatcommands['me'] then
     minetest.chatcommands['me'].func = function(name, param)
         local uncensored = chat_anticurse.check_message(name, param)
 
-        if uncensored == 1 then
-            justice.sentence(judge_name, name, tonumber(seconds), cause)
-            minetest.log('action', 'Player '..name..' warned for '..cause..'. Msg:'..param)
-            return
+        if uncensored == 1 or uncensored ==2 then
+            minetest.chat_send_player(name, 'Please help us keep the chat rated PG, if you want to curse use /channels')
+            minetest.log('action', 'Player '..name..' warned for cursing Msg:'..param)
+            return false
         end
 
-        if uncensored == 2 then
-            justice.sentence(judge_name, name, tonumber(seconds), cause)
-            --minetest.chat_send_all('Player <'..name..'> warned for '..cause..'.' )
-            minetest.log('action', 'Player '..name..' warned for '..cause..'. Me:'..param)
-            return
-        end
-        
-        return old_command(name, param)
-    end
-end
-
-if minetest.chatcommands['msg'] then
-    local old_command = minetest.chatcommands['msg'].func
-    minetest.chatcommands['msg'].func = function(name, param)
-        local uncensored = chat_anticurse.check_message(name, param)
-
-        if uncensored == 1 then
-            justice.sentence(judge_name, name, tonumber(seconds), cause)
-            minetest.log('action', 'Player '..name..' warned for '..cause..'. Msg:'..param)
-            return
-        end
-
-        if uncensored == 2 then
-            justice.sentence(judge_name, name, tonumber(seconds), cause)
-           -- minetest.chat_send_all('Player <'..name..'> warned for '..cause..'' )
-            minetest.log('action', 'Player '..name..' warned for '..cause..'. Msg:'..param)
-            return
-        end
-        
         return old_command(name, param)
     end
 end
